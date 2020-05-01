fe379de9725c76c90d1231e5c2be475bfb460741
Namhyun, Gu <mnhan0403@gmail.com>
2020-05-01 20:12:44 +0900

    Add 'baekjoon/11053' solution

---
+++ baekjoon/11053/solution.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, result = 0;
+  int num[1000];
+  int dp[1000] = {0};
+
+  cin >> N;
+  for (int i = 0; i < N; i++) {
+    cin >> num[i];
+
+    dp[i] = 1;
+    for (int j = 0; j < i; j++) {
+      if (num[i] > num[j]) {
+        dp[i] = max(dp[i], dp[j] + 1);
+      }
+    }
+    result = max(result, dp[i]);
+  }
+  cout << result << endl;
+}
